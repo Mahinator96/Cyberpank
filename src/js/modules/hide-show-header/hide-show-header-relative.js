8f8b4e0996d037eb1551c0675d7afc3e9b2c2cbb
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2023-10-19 22:35:15 +0300

    Header, Hero, About (No adaptiv)

---
+++ src/js/modules/hide-show-header/hide-show-header-relative.js
@@ -0,0 +1,17 @@
+let lastScroll = 0;
+const header = document.querySelector('header');
+const scrollPosition = () =>  window.pageYOffset || document.documentElement.scrollTop;
+const isHeaderHide = () => header.classList.contains('hide');
+
+console.log(header.offsetHeight);
+
+window.addEventListener('scroll', () => {
+	if (scrollPosition() > header.offsetHeight && !isHeaderHide()) {
+		header.classList.add('hide');
+	}
+	if (scrollPosition() < header.offsetHeight && isHeaderHide()) {
+		header.classList.remove('hide');
+	}
+
+	lastScroll = scrollPosition();
+});
